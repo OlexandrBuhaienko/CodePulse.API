9ad6b17dcc7bf3326afd8d0901ca5ff4594bacb5
OlexandrBuhaienko <soulofdeveloper0911@gmail.com>
2024-08-10 22:06:53 +0300

    I have added authentication and I've added the JWT bearer and the settings for the validation parameters
    for the token.
    
    And finally, I have added authentication to the Http pipeline.

--- CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
+++ CodePulse.API/obj/Debug/net7.0/CodePulse.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb8c607f580da9eb4e9cec0e40b0a234f9b9a2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67256c93c27b2c45c556eb7701cb898d2501b337")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
